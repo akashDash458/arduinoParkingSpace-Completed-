a9795badb630e00c7103742078aed7a379b4705b
Akash Dash <akash.dash.vicky@gmail.com>
2017-04-20 18:32:55 +0530

    Updated LCD ports and messages

---
+++ LCDdisplay/LCDdisplay.ino
@@ -0,0 +1,110 @@
+/**This Arduino project uses Arduino Mega 2560 for a 3x2 parking system.
+ * It uses ultrasonic sensors to detect cars, servo motors to open and close gate after scanning Rfid card.
+ * Display is taken care by a 16x2 lcd panel and LCDs to show parking status of each slot. * 
+ * 
+ */
+#include <LiquidCrystal.h>  
+int Contrast=10;
+//Instead of using potentiometer, I ve manually set the contrast
+LiquidCrystal lcd(28, 27, 25, 24, 23, 22);   
+ int slot[2][3]={{0,1,1},{1,0,1}};
+ void setup()
+ { 
+    setupLCD();
+  }  
+void setupLCD()
+{
+    analogWrite(26,Contrast);  //To set the contrast
+    lcd.begin(16, 2);         //To setup no. of columns and rows of lcd panel
+    lcd.print("hello, world!");
+    //pinMode(30, OUTPUT);       //Set pin 9 as output for brightness
+    //analogWrite(30,20);        //Set brightness as 50 (0-255)
+
+}
+
+//This part needs to be  changed once we combine the project  
+void loop()
+ {  
+     dispGateOpen();
+     
+     dispGateClose();
+    
+ }
+//Invoked when the gate opens
+void dispGateOpen()
+{
+  lcd.clear();
+  lcd.print("<<OPENING GATE>>");
+  delay(3000); 
+   
+}
+
+////Invoked when the gate closes
+void dispGateClose()
+{
+  lcd.clear();
+  lcd.setCursor(0,1);
+  lcd.print("<<CLOSING GATE>>");
+  delay(3000);
+  dispParkingStatus();
+  dispNearest(0,0); 
+  
+ }
+
+//This function shows the current parking status. LED status to be added later.
+void dispParkingStatus()
+{
+  lcd.clear();
+  lcd.print("Parking Status :");
+  delay(1000);
+  lcd.clear();
+  int rows=2,columns=3;
+  int i,j;
+  
+  for(i=0;i<rows;i++)
+   { 
+    lcd.setCursor(0,i);
+    for(j=0;j<columns;j++)
+    {
+      if(slot[i][j]==1)
+        {
+          lcd.print("  X  ");
+          
+        }
+      else
+        {
+          lcd.print(" ");
+          lcd.print((char)(j+65));
+          lcd.print(i+1);
+          lcd.print(" ");       
+          
+        }
+    }
+    
+      
+  }
+   delay(3000); 
+    
+}
+
+
+//This function shows nearest slot after taking parameters from calciulateNearest function
+void dispNearest(int r,int c)
+{
+  lcd.clear();
+  
+  lcd.print("Nearest slot: ");
+  char row=(char)(r+65);
+  int col =c+1;
+  float distance = 4.0 + 2.5*c;
+  lcd.print(row);
+  lcd.print(col);
+  
+  lcd.setCursor(0,1);
+  lcd.print("Distance :");
+  lcd.print(distance);
+  lcd.print(" m");
+  delay(3000); 
+  loop();  
+}
+
---
+++ new_update_slot/new_update_slot.ino
@@ -0,0 +1,78 @@
+int slot[3][2];
+
+#define t1 1
+#define t2 2 
+#define t3 3 
+#define t4 4 
+#define t5 5 
+#define t6 6
+#define e1 7
+#define e2 8 
+#define e3 9
+#define e4 10
+#define e5 11
+#define e6 12
+
+void setup()
+{
+  Serial.begin(9600);
+  pinMode(t1,OUTPUT);
+  pinMode(t2,OUTPUT);
+  pinMode(t3,OUTPUT);
+  pinMode(t4,OUTPUT);
+  pinMode(t5,OUTPUT);
+  pinMode(t6,OUTPUT);
+  pinMode(e1,INPUT);
+  pinMode(e2,INPUT);
+  pinMode(e3,INPUT);
+  pinMode(e4,INPUT);
+  pinMode(e5,INPUT);
+  pinMode(e6,INPUT);
+}
+
+
+void loop()
+{
+  int ti1,ti2,ti3,ti4,ti5,ti6;
+  digitalWrite(t1,HIGH);
+  delay(100);
+  digitalWrite(t1,LOW);
+  ti1=pulseIn(e1,HIGH);
+  slot[0][0]=ti1/58.2;
+  
+  digitalWrite(t2,HIGH);
+  delay(100);
+  digitalWrite(t2,LOW);
+  ti2=pulseIn(e2,HIGH);
+  slot[0][1]=ti2/58.2;
+
+  digitalWrite(t3,HIGH);
+  delay(100);
+  digitalWrite(t3,LOW);
+  ti3=pulseIn(e3,HIGH);
+  slot[1][0]=ti3/58.2;
+
+  digitalWrite(t4,HIGH);
+  delay(100);
+  digitalWrite(t4,LOW);
+  ti4=pulseIn(e4,HIGH);
+  slot[1][1]=ti4/58.2;
+
+   digitalWrite(t5,HIGH);
+  delay(100);
+  digitalWrite(t5,LOW);
+  ti5=pulseIn(e5,HIGH);
+  slot[2][0]=ti5/58.2;
+
+   digitalWrite(t6,HIGH);
+  delay(100);
+  digitalWrite(t6,LOW);
+  ti6=pulseIn(e6,HIGH);
+  slot[2][1]=ti6/58.2;
+}
+
+int updateSlot(int i,int j)
+{
+  return slot[i][j];
+}
+
