3fa53e1c8e4f4784faa4af3083962289260713d9
Akash Dash <akash.dash.vicky@gmail.com>
2017-04-23 14:28:59 +0530

    modified function calls

--- calcNearest/calcNearest.ino
+++ calcNearest/calcNearest.ino
@@ -1,40 +1,39 @@
 /*Initialse all the variables and have the slot set to zero bydefault
  * When slot is full have the sensor send voltage change to the ide and result will change.
  */
-int m=3,n=2,i=0,j=0,flag=1;   //declare the variables
-int slot[3][2];   //declare the slot as 2d array
+int row=2,col=3,flag=1;   //declare the variables
+int slot[2][3];   //declare the slot as 2d array
 
 void setup() {
   // put your setup code here, to run once:
   
 }
 
-void loop(
-  ) {
-  // put your main code here, to run repeatedly:
-    for(i=0;i<m;i++)
-     for(j=0;j<n;j++)
-     {
-      //get the inputs here
-     }
-    for(i=0;i<m;i++)//loop across rows
+void loop() 
+{
+    calcNearest();
+}
+
+void calcNearest()
+{
+  int i,j;
+  for(j=0;j<col;j++)//loop across columns
     {
-     for(j=0;j<n;j++)      //loop across columns
+     for(i=0;i<row;i++)      //loop across rows
      {
       if(slot[i][j]==0)//check availability of slot
       {
-       flag=0;             //tells that empty slot is there
-       goto found;         //directs to the display function
+         flag=0;             //tells that empty slot is there
+         break;         //break out of the loop
       }
      }
     }
-    if(flag==1)            //if no spot is found
-    {
-     print_it(flag);       //slot not found
-    }
-    else
-    {
-    found:
-     print_it(i,j);        //send index to display panel
-    }
+    
+  if(flag==1)            //if no spot is found
+    dispFull();       //slot not found
+    
+  else
+    dispNearest(i,j);        //send index to display panel
+   
 }
+
