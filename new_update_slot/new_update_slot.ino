298e392a4170a43161b911993ba7413d69fb1db2
Akash Dash <akash.dash.vicky@gmail.com>
2017-04-24 23:02:08 +0530

    updated input algo

--- new_update_slot/new_update_slot.ino
+++ new_update_slot/new_update_slot.ino
@@ -1,8 +1,4 @@
-
-
-int slot[2][3]={{0,0,0},{0,0,0}};
-int ledMatrix[2][3] = { {43,  44,  45  }, {46,  47,  48  } };
-
+int slot[2][3];
 
 #define t1 31
 #define t2 32 
@@ -19,15 +15,12 @@ int ledMatrix[2][3] = { {43,  44,  45  }, {46,  47,  48  } };
 
 //the matrix will hold the pin assignments for the led
  
-
-void setup()
-{
-  setupUltrasonic();
-  setupLED();
+int ledMatrix[2][3] = {
  
-}
-
-void setupUltrasonic()
+  {43,  44,  45  },
+  {46,  47,  48  }
+};
+void setup()
 {
   Serial.begin(9600);
   pinMode(t1,OUTPUT);
@@ -42,95 +35,75 @@ void setupUltrasonic()
   pinMode(e4,INPUT);
   pinMode(e5,INPUT);
   pinMode(e6,INPUT);
-}
 
-void setupLED()
-{
   //Initialse the LED's
-  for(int i = 0; i < 2; i++)
-  {
-    for(int j = 0; j < 3; j++)
-    {
+  for(int i = 0; i < 2; i++){
+ 
+    for(int j = 0; j < 3; j++){
+ 
       pinMode(ledMatrix[i][j], OUTPUT);
  
     }//close for i
  
   }//close for j
 }
- 
+
+
 void loop()
 {
-  updateSlot();
-}
-
-void updateSlot()
-{
-  int ti1,ti2,ti3,ti4,ti5,ti6;
-  int arr_ti[6]={ti1,ti2,ti3,ti4,ti5,ti6};//declared the array of ti
-  int arr_e[6]={e1,e2,e3,e4,e5,e6};//declared the array of e
-  int arr_t[6]={t1,t2,t3,t4,t5,t6};//declared the array of t
+  /*
+   * Changes made:-
+   * 1)Removed the array of ti's and integer of ti's and replaced them by required ones.
+   * 2)Added some comments and indented the code.
+   * 
+   * Error showing:- lcd was not declared in scope.
+   * 
+   * Please remove this comment after this has served its purpose.
+   */
+  int arr_e[6]={e1,e2,e3,e4,e5,e6};//declared the array of echo
+  int arr_t[6]={t1,t2,t3,t4,t5,t6};//declared the array of transmitter
   int i=0,j=0,k=1;//declared counter variables 
   int row=2,col=3;//size of parking space
+  int n=0;//initialse the index of array to be zero
+  float dist=0.0,dur=0.0;
   
-   for(j=0;j<col;i++)//loop across columns
+   for(j=0;j<col;j++)//loop across columns
     {
      for(i=0;i<row;i++)      //loop across rows
      {
+      digitalWrite(arr_t[n], LOW);//set the LED off
+      delayMicroseconds(2);
+      lcd.clear();
+      lcd.print("col=");
+      lcd.print(j);
+      lcd.print(" row=");
+      lcd.print(i);
+      delay(1000);//waits for a second
       digitalWrite(arr_t[n],HIGH);
-      delay(100);
+      delayMicroseconds(10);
       digitalWrite(arr_t[n],LOW);
-      arr_ti[n]=pulseIn(arr_e[n],HIGH);
-      slot[i][j]=arr_ti[n]/58.2;
-      if(slot[i][j]<=10)
+      dur=pulseIn(arr_e[n],HIGH);//store the length of pulse for pin
+      dist = dur*.034/2;
+      if(dist<=5&&dist>0)//check if slot is empty or full
       {
-        slot[i][j]=1;
-        digitalWrite(ledMatrix[i][j], HIGH);
+        slot[i][j]=1;//indicate that the slot is filled
+        digitalWrite(ledMatrix[i][j], HIGH);//set led on for that slot 
+        lcd.setCursor(0,1);
+        lcd.print(dist);
+        delay(1000);
       }
       else
       {
-        slot[i][j]=0;
-        digitalWrite(ledMatrix[i][j], LOW);
+        slot[i][j]=0;//indicates the slot is empty
+        digitalWrite(ledMatrix[i][j], LOW);//set led off for that slot 
       }
-      k++;
+      n++;//increment the index of arrays e and t
     }
-  }
-  /*
-  digitalWrite(t1,HIGH);
-  delay(100);
-  digitalWrite(t1,LOW);
-  ti1=pulseIn(e1,HIGH);
-  slot[0][0]=ti1/58.2;
-  
-  digitalWrite(t2,HIGH);
-  delay(100);
-  digitalWrite(t2,LOW);
-  ti2=pulseIn(e2,HIGH);
-  slot[0][1]=ti2/58.2;
 
-  digitalWrite(t3,HIGH);
-  delay(100);
-  digitalWrite(t3,LOW);
-  ti3=pulseIn(e3,HIGH);
-  slot[0][2]=ti3/58.2;
-
-  digitalWrite(t4,HIGH);
-  delay(100);
-  digitalWrite(t4,LOW);
-  ti4=pulseIn(e4,HIGH);
-  slot[1][0]=ti4/58.2;
-
-   digitalWrite(t5,HIGH);
-  delay(100);
-  digitalWrite(t5,LOW);
-  ti5=pulseIn(e5,HIGH);
-  slot[1][1]=ti5/58.2;
-
-   digitalWrite(t6,HIGH);
-  delay(100);
-  digitalWrite(t6,LOW);
-  ti6=pulseIn(e6,HIGH);
-  slot[1][2]=ti6/58.2;
-  */
 }
 
+int updateSlot(int i,int j)
+{
+  return slot[i][j];
+}
 
